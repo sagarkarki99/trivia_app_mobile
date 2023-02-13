ed0491811c603a3037ad53bd2c160b85914dbdc4
sagarkarki99 <sagarkarki184@gmail.com>
2023-02-13 22:47:58 +0545

    add theme color

--- lib/presentation/ui_config/app_colors.dart
+++ lib/presentation/ui_config/app_colors.dart
@@ -14,9 +14,10 @@ class _$AppColors {
   ];
 
   static List<Color> white = [const Color(0XFFFFFFFF)];
+  static List<Color> dark = [const Color(0xFF1F2633)];
+  static List<Color> red = [const Color(0xFFD81F26)];
+  static List<Color> grey = [const Color(0xFFF0F3F5)];
+  static List<Color> lightBrown = [const Color(0xFFF1EDEA)];
+  static List<Color> lightGreen = [const Color(0xFFBAD9D7)];
   static List<Color> disabled = [const Color(0xFFDDDDDD)];
-  static List<Color> green1 = [const Color(0xFF0097A7)];
-  static List<Color> green2 = [const Color(0xFF00796B)];
-  static List<Color> green3 = [const Color(0xFF388E3C)];
-  static List<Color> green4 = [const Color(0xFF689F38)];
 }
--- lib/presentation/ui_config/app_colors.tailor.dart
+++ lib/presentation/ui_config/app_colors.tailor.dart
@@ -13,31 +13,34 @@ class AppColors extends ThemeExtension<AppColors> {
     required this.background,
     required this.primary,
     required this.white,
+    required this.dark,
+    required this.red,
+    required this.grey,
+    required this.lightBrown,
+    required this.lightGreen,
     required this.disabled,
-    required this.green1,
-    required this.green2,
-    required this.green3,
-    required this.green4,
   });
 
   final Color background;
   final Color primary;
   final Color white;
+  final Color dark;
+  final Color red;
+  final Color grey;
+  final Color lightBrown;
+  final Color lightGreen;
   final Color disabled;
-  final Color green1;
-  final Color green2;
-  final Color green3;
-  final Color green4;
 
   static final AppColors light = AppColors(
     background: _$AppColors.background[0],
     primary: _$AppColors.primary[0],
     white: _$AppColors.white[0],
+    dark: _$AppColors.dark[0],
+    red: _$AppColors.red[0],
+    grey: _$AppColors.grey[0],
+    lightBrown: _$AppColors.lightBrown[0],
+    lightGreen: _$AppColors.lightGreen[0],
     disabled: _$AppColors.disabled[0],
-    green1: _$AppColors.green1[0],
-    green2: _$AppColors.green2[0],
-    green3: _$AppColors.green3[0],
-    green4: _$AppColors.green4[0],
   );
 
   static final themes = [
@@ -49,21 +52,23 @@ class AppColors extends ThemeExtension<AppColors> {
     Color? background,
     Color? primary,
     Color? white,
+    Color? dark,
+    Color? red,
+    Color? grey,
+    Color? lightBrown,
+    Color? lightGreen,
     Color? disabled,
-    Color? green1,
-    Color? green2,
-    Color? green3,
-    Color? green4,
   }) {
     return AppColors(
       background: background ?? this.background,
       primary: primary ?? this.primary,
       white: white ?? this.white,
+      dark: dark ?? this.dark,
+      red: red ?? this.red,
+      grey: grey ?? this.grey,
+      lightBrown: lightBrown ?? this.lightBrown,
+      lightGreen: lightGreen ?? this.lightGreen,
       disabled: disabled ?? this.disabled,
-      green1: green1 ?? this.green1,
-      green2: green2 ?? this.green2,
-      green3: green3 ?? this.green3,
-      green4: green4 ?? this.green4,
     );
   }
 
@@ -74,11 +79,12 @@ class AppColors extends ThemeExtension<AppColors> {
       background: Color.lerp(background, other.background, t)!,
       primary: Color.lerp(primary, other.primary, t)!,
       white: Color.lerp(white, other.white, t)!,
+      dark: Color.lerp(dark, other.dark, t)!,
+      red: Color.lerp(red, other.red, t)!,
+      grey: Color.lerp(grey, other.grey, t)!,
+      lightBrown: Color.lerp(lightBrown, other.lightBrown, t)!,
+      lightGreen: Color.lerp(lightGreen, other.lightGreen, t)!,
       disabled: Color.lerp(disabled, other.disabled, t)!,
-      green1: Color.lerp(green1, other.green1, t)!,
-      green2: Color.lerp(green2, other.green2, t)!,
-      green3: Color.lerp(green3, other.green3, t)!,
-      green4: Color.lerp(green4, other.green4, t)!,
     );
   }
 
@@ -91,11 +97,14 @@ class AppColors extends ThemeExtension<AppColors> {
                 .equals(background, other.background) &&
             const DeepCollectionEquality().equals(primary, other.primary) &&
             const DeepCollectionEquality().equals(white, other.white) &&
-            const DeepCollectionEquality().equals(disabled, other.disabled) &&
-            const DeepCollectionEquality().equals(green1, other.green1) &&
-            const DeepCollectionEquality().equals(green2, other.green2) &&
-            const DeepCollectionEquality().equals(green3, other.green3) &&
-            const DeepCollectionEquality().equals(green4, other.green4));
+            const DeepCollectionEquality().equals(dark, other.dark) &&
+            const DeepCollectionEquality().equals(red, other.red) &&
+            const DeepCollectionEquality().equals(grey, other.grey) &&
+            const DeepCollectionEquality()
+                .equals(lightBrown, other.lightBrown) &&
+            const DeepCollectionEquality()
+                .equals(lightGreen, other.lightGreen) &&
+            const DeepCollectionEquality().equals(disabled, other.disabled));
   }
 
   @override
@@ -105,11 +114,12 @@ class AppColors extends ThemeExtension<AppColors> {
         const DeepCollectionEquality().hash(background),
         const DeepCollectionEquality().hash(primary),
         const DeepCollectionEquality().hash(white),
-        const DeepCollectionEquality().hash(disabled),
-        const DeepCollectionEquality().hash(green1),
-        const DeepCollectionEquality().hash(green2),
-        const DeepCollectionEquality().hash(green3),
-        const DeepCollectionEquality().hash(green4));
+        const DeepCollectionEquality().hash(dark),
+        const DeepCollectionEquality().hash(red),
+        const DeepCollectionEquality().hash(grey),
+        const DeepCollectionEquality().hash(lightBrown),
+        const DeepCollectionEquality().hash(lightGreen),
+        const DeepCollectionEquality().hash(disabled));
   }
 }
 
@@ -118,9 +128,10 @@ extension AppColorsBuildContextProps on BuildContext {
   Color get background => _appColors.background;
   Color get primary => _appColors.primary;
   Color get white => _appColors.white;
+  Color get dark => _appColors.dark;
+  Color get red => _appColors.red;
+  Color get grey => _appColors.grey;
+  Color get lightBrown => _appColors.lightBrown;
+  Color get lightGreen => _appColors.lightGreen;
   Color get disabled => _appColors.disabled;
-  Color get green1 => _appColors.green1;
-  Color get green2 => _appColors.green2;
-  Color get green3 => _appColors.green3;
-  Color get green4 => _appColors.green4;
 }
